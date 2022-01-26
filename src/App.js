74b817b2aa9c7eba4a50e512fac5b8a8e1497a9e
Xin Chen <xinchen@NCSC02F91KZQ05F.local>
2022-01-26 11:29:22 +0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a-scene
+        embedded
+        arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;">
+        <a-assets>
+          <a-asset-item
+            id="animated-asset"
+            src="https://raw.githubusercontent.com/nicolocarpignoli/nicolocarpignoli.github.io/master/ar-playground/models/CesiumMan.gltf"></a-asset-item>
+        </a-assets>
+
+        <a-marker type="barcode" value="7">
+          <a-box position="0 0.5 0" color="blue"></a-box>
+        </a-marker>
+
+        <a-marker id="animated-marker" type="barcode" value="6">
+          <a-entity gltf-model="#animated-asset" scale="2"></a-entity>
+        </a-marker>
+
+        <a-entity camera></a-entity>
+      </a-scene>
+    </>
   );
 }
 
