002299395b253080cadf8f0a75da0a1a193a155f
Xin Chen <xinchen@NCSC02F91KZQ05F.local>
2022-01-27 09:23:14 +0800

    change file path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import pattFile from './pattern-2359.patt';
+import robot from './robot.glb';
 
 function App() {
   return (
     <>
       <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
         <a-assets>
-          <a-asset-item
-            id="robot"
-            src="https://skarredghost.com/AR/BrainStem.glb"></a-asset-item>
+          <a-asset-item id="robot" src={robot}></a-asset-item>
         </a-assets>
 
         <a-marker
