686d98505c37b05c2fbe90740d8881fc6c22843e
Xin Chen <xinchen@NCSC02F91KZQ05F.local>
2022-01-27 09:20:26 +0800

    change model

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import pattFile from './pattern-2359.patt';
 function App() {
   return (
     <>
-      <a-scene
-        embedded
-        arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;">
+      <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
         <a-assets>
           <a-asset-item
-            id="animated-asset"
-            src="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"></a-asset-item>
+            id="robot"
+            src="https://skarredghost.com/AR/BrainStem.glb"></a-asset-item>
         </a-assets>
 
-        <a-marker id="animated-marker" type="pattern" url={pattFile}>
-          <a-entity gltf-model="#animated-asset" scale="2"></a-entity>
+        <a-marker
+          id="animated-marker"
+          preset="custom"
+          type="pattern"
+          url={pattFile}>
+          <a-entity gltf-model="#robot" animation-mixer></a-entity>
         </a-marker>
 
         <a-entity camera></a-entity>
