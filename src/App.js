605766f0e47a65fb6aaab8c490c8ec4b7f47db10
Xin Chen <xinchen@NCSC02F91KZQ05F.local>
2022-01-27 09:34:32 +0800

    update github links

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
         <a-assets>
           <a-asset-item
             id="robot"
-            src="https://cdn.statically.io/gh/cxin7/react-ar/blob/master/src/robot.glb"></a-asset-item>
+            src="https://cdn.statically.io/gh/cxin7/react-ar/master/src/robot.glb"></a-asset-item>
         </a-assets>
 
         <a-marker
           id="animated-marker"
           preset="custom"
           type="pattern"
-          url="https://cdn.statically.io/gh/cxin7/react-ar/blob/master/src/pattern-2359.patt">
+          url="https://cdn.statically.io/gh/cxin7/react-ar/master/src/pattern-2359.patt">
           <a-entity gltf-model="#robot" animation-mixer></a-entity>
         </a-marker>
 
