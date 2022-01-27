dd81b67ae88e1b7d7dab4caa8c48bcfa24ef7e43
Xin Chen <xinchen@NCSC02F91KZQ05F.local>
2022-01-27 09:30:48 +0800

    change file path to github links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import pattFile from './pattern-2359.patt';
-import robot from './robot.glb';
 
 function App() {
   return (
     <>
       <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false;">
         <a-assets>
-          <a-asset-item id="robot" src={robot}></a-asset-item>
+          <a-asset-item
+            id="robot"
+            src="https://cdn.statically.io/gh/cxin7/react-ar/blob/master/src/robot.glb"></a-asset-item>
         </a-assets>
 
         <a-marker
           id="animated-marker"
           preset="custom"
           type="pattern"
-          url={pattFile}>
+          url="https://cdn.statically.io/gh/cxin7/react-ar/blob/master/src/pattern-2359.patt">
           <a-entity gltf-model="#robot" animation-mixer></a-entity>
         </a-marker>
 
