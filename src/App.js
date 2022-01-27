d1be2d3cc2bae0b2a64a07ad4ba3302a6d04be5a
Xin Chen <xinchen@NCSC02F91KZQ05F.local>
2022-01-27 10:28:31 +0800

    remove custom scene

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <>
-      <a-scene
-        embedded
-        arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;">
-        <a-marker type="barcode" value="2">
-          <a-entity
-            position="2 2 0"
-            scale="0.05 0.05 0.05"
-            gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"></a-entity>
-        </a-marker>
-
-        <a-entity camera></a-entity>
-      </a-scene>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
